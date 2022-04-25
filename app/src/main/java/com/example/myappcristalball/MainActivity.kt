782ebd05133aa7bb00ee91c66a36f5225e964f55
KMV <9117479@gmail.com>
2022-04-25 21:26:56 +0300

    NC App Cristal Ball Android Studio

--- app/src/main/java/com/example/myappcristalball/MainActivity.kt
+++ app/src/main/java/com/example/myappcristalball/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myappcristalball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myappcristalball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textViewPrediction.setOnClickListener {
+            binding.textViewPrediction.text = getPrediction()
+        }
+    }
+    private fun randomNumber(): Int{
+        val sizeArray = resources.getStringArray(R.array.prediction).size - 1
+        return (0..sizeArray).random()
+    }
+    private fun getPrediction(): String{
+        return resources.getStringArray(R.array.prediction)[randomNumber()]
     }
 }
